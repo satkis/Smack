c8d12f9b3bb306eb031748dc99cad18fd0e9701a
satkis <satkis@satkiss-Mac.local>
2017-12-28 00:29:37 +0200

    VC side slider; menuBtn
    	modified:   Pods/Pods.xcodeproj/xcuserdata/satkis.xcuserdatad/xcschemes/xcschememanagement.plist
    	modified:   Smack.xcodeproj/project.pbxproj
    	modified:   Smack.xcodeproj/xcuserdata/satkis.xcuserdatad/xcschemes/xcschememanagement.plist
    	modified:   Smack/Base.lproj/Main.storyboard
    	new file:   Smack/Controller/ChannelVC.swift
    	new file:   Smack/Controller/ChatVC.swift
    	deleted:    Smack/ViewController.swift

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by satkis on 12/27/17.
+//  Copyright © 2017 satkis. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 70
+    }
+
+   
+    
+
+}
---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,30 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by satkis on 12/27/17.
+//  Copyright © 2017 satkis. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //this is outlet(not action) because actions included manually in viewdidload
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action:
+            //revealToggle to slide out for anotherViewController. like for CityBee app when car selected
+            #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        //slide with finger viewController from a side
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        //tap anywhere to close slided viewcontroller
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+    
+    
+}
