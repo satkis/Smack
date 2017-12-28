d67fb2207808f9bf87dda9c58ba751cb46c8722d
satkis <satkis@satkiss-Mac.local>
2017-12-28 22:30:52 +0200

    connected to Server. began with API

---
+++ Smack/Services/AuthService.swift
@@ -0,0 +1,79 @@
+//
+//  AuthService.swift
+//  Smack
+//
+//  Created by satkis on 12/28/17.
+//  Copyright © 2017 satkis. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+//this spot handles login, create, register user functions
+
+
+//this is singleton class. Singleton: In object-oriented programming , a singleton class is a class that can have only one object (an instance of the class) at a time.
+//class accessible globally and only one instance available at a time
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn: Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken: String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail: String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let header = [
+            "Content-Type": "application/json; characterset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            
+            if response.result.error == nil {
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+                
+            }
+        }
+        
+    }
+    
+    
+    
+    
+}
--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,9 +8,24 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_SIGNUP = "ToSignUp"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+//URLs
+
+let BASE_URL = "https://smaksatkis.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
+
 
 let SHADOW_COLOR: CGFloat = 157.0 / 255.0
