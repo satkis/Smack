041852b22cd07d9829a49fa25043f9299ec93e9b
satkis <satkis@satkiss-Mac.local>
2018-01-20 16:42:06 +0200

    Final - fixed messages' timeStamp

--- Smack/View/MessageCell.swift
+++ Smack/View/MessageCell.swift
@@ -26,6 +26,27 @@ class MessageCell: UITableViewCell {
         userNameLbl.text = message.username
         userImg.image = UIImage(named: message.userAvatar)
         userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+        
+        
+        guard var isoDate = message.timeStamp else { return }
+      //  let start = isoDate.index(isoDate.startIndex, offsetBy: 1)
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        //isoDate = isoDate.substring(to: end)
+        let isoDatee = isoDate[..<end]
+        //isoDate = Substring(to: end)
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDatee.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStampLbl.text = finalDate
+        }
+        
+        
     }
     
     
